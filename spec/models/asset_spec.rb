62bb9861ca02ac9193a00d927ff384379fb597cc
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-08-16 19:40:57 -0300

    chore: update description test

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -1,18 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Asset, type: :model do
-  describe "Asset" do
-    subject(:asset) { described_class.new }
+  subject(:asset) { described_class.new }
 
-    it "is valid with valid attributes" do
-      asset.name = "test"
-      asset.code = "test"
-      asset.category = "test"
-      expect(asset).to be_valid
-    end
+  it "is valid with valid attributes" do
+    asset.name = "test"
+    asset.code = "test"
+    asset.category = "test"
+    expect(asset).to be_valid
+  end
 
-    it "is invalid without valid attributes" do
-      expect(asset).to be_invalid
-    end
+  it "is invalid without valid attributes" do
+    expect(asset).to be_invalid
   end
 end
