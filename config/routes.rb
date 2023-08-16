29b38048a0901b709da4ced281a9586ba06e5413
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-08-15 22:25:55 -0300

    feat: add route asset

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :things
-  root "things#index"
+  resources :assets, only: %i[create index update show]
 end
